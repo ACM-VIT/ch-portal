2fa207033d1c435c61268edb9a82b8fe1416bbe5
Anand Rajaram <anandrajaram21@gmail.com>
2023-09-23 05:26:36 +0530

    feat: added some cool stuff ig

---
+++ src/components/CountdownTimer.tsx
@@ -0,0 +1,7 @@
+export default function CountdownTimer() {
+  return (
+    <div>
+      <h1>Countdown Timer</h1>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { QuestionContextProvider } from "@/contexts/questions";
-import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
 import type { AppProps } from "next/app";
+
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QuestionContextProvider>
       <Component {...pageProps} />
+      <ToastContainer />
     </QuestionContextProvider>
   );
 }
